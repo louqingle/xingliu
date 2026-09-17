45dfb64a0052ae7d1b96063f29b37e827fe5b863
Srtie <2144572907@qq.com>
2026-09-18 00:53:02 +0800

    feat: switch upload page to Douyin-style studio

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,5 +1,5 @@
-import NoZoomStudio from "./no-zoom";
+import StudioDouyin from "./studio-douyin";
 
 export default function UploadPage() {
-  return <NoZoomStudio />;
+  return <StudioDouyin />;
 }
